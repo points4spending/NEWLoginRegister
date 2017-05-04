487fe1d23ba9ae5aacf1002e51fa96e38aefaadb
points4spending <rhyspayne1999@gmail.com>
2017-05-04 21:22:51 +0100

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,12 +1,12 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("localhost", "id1553988_points4spending", "rhyspayne1999", "id1553988_points4spending");
     
     $name = $_POST["name"];
     $age = $_POST["age"];
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
+    $statement = mysqli_prepare($con, "INSERT INTO user (FirstName LastName ,username, age, password) VALUES (?, ?, ?, ?)");
     mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
     mysqli_stmt_execute($statement);
     
